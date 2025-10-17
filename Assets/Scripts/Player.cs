2c75a47f078a51910b64f894c51efd46cba18bd1
eoin <eoin.corrigan.j94981@mcast.edu.mt>
2025-10-17 12:26:28 +0200

    Moving Character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Move()
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float padding = 0.1f;
+
+    float xMin, xMax, yMin, yMax;
+
+    void Start()
     {
-        var deltaX = Input.GetAxis(“Horizontal”); 
-        var newXPos = transform.position.x + deltaX;
-        transform.position = new Vector2(newXPos, transform.position.y);
+        SetUpMoveBoundaries();
+    }
+    public void Update()
+    {
+        Move();
     }
 
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
 
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
 }
