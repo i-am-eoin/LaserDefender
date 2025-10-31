4426b34c90d2da97547929d1d4251a7539c6b429
eoin <eoin.corrigan.j94981@mcast.edu.mt>
2025-10-31 12:12:41 +0100

    ToGoPublic

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+    void Update()
+    {
+        EnemyMove();
+    }
+    
+    void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count)
+        {
+        var targetPosition = waypoints[waypointIndex].transform.position;        
+        targetPosition.z = 0f;
+        
+        var movementThisFrame = moveSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+        if (transform.position == targetPosition)
+        {
+            waypointIndex++;
+        }
+        } else {
+            Destroy(gameObject);
+        }
+    }
 }
