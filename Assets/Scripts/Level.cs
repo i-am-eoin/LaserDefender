61e08fcc07d240c55bae02f5b8f78f145aa36ec6
eoin <eoin.corrigan.j94981@mcast.edu.mt>
2025-12-05 11:50:24 +0100

    Large update
    Level feature added (start menu, game)
    Starfield particles added.

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }   
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LaserDefender");
+    }
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
